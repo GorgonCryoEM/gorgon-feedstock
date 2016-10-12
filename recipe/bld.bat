85f6915d18556bf40dc91d9a250127479ae06e87
shadow_walker <shadowwalkersb@gmail.com>
2016-10-12 14:51:45 -0500

    -DCMAKE_BUILD_TYPE=Release

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ if errorlevel 1 exit 1
 cd %builddir%
 if errorlevel 1 exit 1
 
-cmake %SRC_DIR% -G "NMake Makefiles"
+cmake %SRC_DIR% -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release
 if errorlevel 1 exit 1
 
 cmake --build %builddir% --config Release --target install
