5cc3a91e1e19ee6303f84b77abd589fa4eb00f49
shadow_walker <shadowwalkersb@gmail.com>
2016-10-18 01:18:45 -0500

    /opt/conda/pkgs/

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,9 @@ cd $build_dir
 which -a cmake
 
 find ${PREFIX} -iname "libarch*"
+pkg_files=( `find /opt/conda/pkgs/ -iname "libarch*"` )
 
-files=( $PREFIX/bin/cmake `ls $PREFIX/lib/libarch*.so*` )
+files=( $PREFIX/bin/cmake `ls $PREFIX/lib/libarch*.so*` ${pkg_files[@]} )
 
 for f in ${files[@]};do
     ls -l $f
