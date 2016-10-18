3e109edb375e0be16710455937fa8675cbc7a7e7
shadow_walker <shadowwalkersb@gmail.com>
2016-10-17 23:00:17 -0500

    find libarchive

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,8 @@ rm -rf $build_dir
 mkdir -p $build_dir
 cd $build_dir
 
-cmake $SRC_DIR
-cmake --build ${build_dir} --config Release --target install -- -j
+which -a cmake
 
-$PYTHON setup.py install
+find ${PREFIX} -iname "libarch*"
+
+cmake --version
