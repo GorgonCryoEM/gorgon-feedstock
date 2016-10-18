ce1d0a48a05e5a99691a33304b5a7c7d61c82b18
shadow_walker <shadowwalkersb@gmail.com>
2016-10-18 01:15:02 -0500

    only ls -l, no decorations

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,18 +13,7 @@ find ${PREFIX} -iname "libarch*"
 files=( $PREFIX/bin/cmake `ls $PREFIX/lib/libarch*.so*` )
 
 for f in ${files[@]};do
-    echo "=========== $f ============="
     ls -l $f
-    objdump -x $f | grep RPATH
-    ldd $f
-    echo "======================================="
 done
 
-ltrace -S -C ${PREFIX}/bin/cmake --version
-
-${PREFIX}/bin/cmake --version
-${PREFIX}/bin/cmake $SRC_DIR
-${PREFIX}/bin/cmake --build ${build_dir} --config Release --target install -- -j
- 
-$PYTHON setup.py install
-
+cmake --version
