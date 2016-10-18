b7a0fb2ab86e73b02030b2ff204a76a09c15c7df
shadow_walker <shadowwalkersb@gmail.com>
2016-10-18 12:23:47 -0500

    export LD_LIBRARY_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,8 @@ rm -rf $build_dir
 mkdir -p $build_dir
 cd $build_dir
 
+export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
+
 cmake $SRC_DIR
 cmake --build ${build_dir} --config Release --target install -- -j
 
